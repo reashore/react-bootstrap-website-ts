914ef2117fab964cdd58756e831f39e50854def2
Frank J. Reashore <reashore@telus.net>
2019-02-22 23:19:01 -0800

    Used SFCs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
@@ -7,20 +7,16 @@ import Home from "./Components/Home/Home";
 import MenuBar from "./Components/MenuBar/MenuBar";
 import News from "./Components/News/News";
 
-class App extends Component {
-  public render() {
-    return (
-      <Router>
-        <Container>
-          <MenuBar />
-          <Route path="/" exact component={Home} />
-          <Route path="/Home" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/news" component={News} />
-        </Container>
-      </Router>
-    );
-  }
-}
+const App: React.SFC = () => (
+  <Router>
+    <Container>
+      <MenuBar />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/Home" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/news" component={News} />
+    </Container>
+  </Router>
+);
 
 export default App;
--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,49 +1,56 @@
-import React, { Component } from "react";
+import React from "react";
 import { Button, Col, Container, Image, Jumbotron, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./Home.css";
 
-export default class Home extends Component {
-  public render() {
-    return (
-      <Container>
-        <Jumbotron>
-          <h2>Welcome to CodeLife</h2>
-          <p>A website built with React, React-Router, and React-Bootstrap</p>
-          <Link to="/about">
-            <Button variant="primary">Learn More</Button>
-          </Link>
-        </Jumbotron>
-        <Row className="show-grid text-center">
-          <Col xs={12} sm={4} className="person-wrapper">
-            <Image src="assets/person-1.jpg" rounded className="profile-pic" />
-            <h3>Frank</h3>
-            <p>
-              That's a crooked tree. We'll send him to Washington. These little
-              son of a guns hide in your brush and you just have to push them
-              out.
-            </p>
-          </Col>
-          <Col xs={12} sm={4} className="person-wrapper">
-            <Image src="assets/person-2.jpg" rounded className="profile-pic" />
-            <h3>Vanessa</h3>
-            <p>
-              That's a crooked tree. We'll send him to Washington. These little
-              son of a guns hide in your brush and you just have to push them
-              out.
-            </p>
-          </Col>
-          <Col xs={12} sm={4} className="person-wrapper">
-            <Image src="assets/person-3.jpg" rounded className="profile-pic" />
-            <h3>Riff</h3>
-            <p>
-              That's a crooked tree. We'll send him to Washington. These little
-              son of a guns hide in your brush and you just have to push them
-              out.
-            </p>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+const Home: React.SFC = () => (
+  <Container>
+    <Jumbotron>
+      <h2>Welcome to CodeLife</h2>
+      <p>A website built with React, React-Router, and React-Bootstrap</p>
+      <Link to="/about">
+        <Button variant="primary">Learn More</Button>
+      </Link>
+    </Jumbotron>
+    <Row className="show-grid text-center">
+      <Col xs={12} sm={4} className="person-wrapper">
+        <Image
+          src="assets/person-1.jpg"
+          rounded={true}
+          className="profile-pic"
+        />
+        <h3>Frank</h3>
+        <p>
+          That's a crooked tree. We'll send him to Washington. These little son
+          of a guns hide in your brush and you just have to push them out.
+        </p>
+      </Col>
+      <Col xs={12} sm={4} className="person-wrapper">
+        <Image
+          src="assets/person-2.jpg"
+          rounded={true}
+          className="profile-pic"
+        />
+        <h3>Vanessa</h3>
+        <p>
+          That's a crooked tree. We'll send him to Washington. These little son
+          of a guns hide in your brush and you just have to push them out.
+        </p>
+      </Col>
+      <Col xs={12} sm={4} className="person-wrapper">
+        <Image
+          src="assets/person-3.jpg"
+          rounded={true}
+          className="profile-pic"
+        />
+        <h3>Riff</h3>
+        <p>
+          That's a crooked tree. We'll send him to Washington. These little son
+          of a guns hide in your brush and you just have to push them out.
+        </p>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default Home;
